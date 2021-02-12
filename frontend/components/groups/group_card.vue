7bc3f69bfb0d41a4c199688f54adcdb30f633aac
João Marcos <joao.marcos@buser.com.br>
2021-02-12 13:15:50 -0300

    Componentiza os cards de grupos

---
+++ frontend/components/groups/group_card.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card light class="ma-3 card" height="300" width="400" hover>
+    <v-card-title class="card-title">{{ group.title }}</v-card-title>
+    <v-card-text class="ma-0 pt-0 pb-1 pl-7 pr-7" v-for="item in group.items" :key="item">
+      <img class="software-logo" src="~/static/software/office.png">
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  props: ['group'],
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+    .card:hover {
+        cursor: default;
+    }
+  .card-title {
+      font-weight: 300;
+      font-size: 24px;
+      color: #5B5B5B;
+  }
+  .software-logo {
+      height: 20px;
+      width: auto;
+  }
+</style>
--- frontend/pages/softwares.vue
+++ frontend/pages/softwares.vue
@@ -1,17 +1,41 @@
 <template>
-  <v-layout justify-center align-center>
-    gerenciamento de softwares
+  <v-layout justify-start align-start>
+    <div v-for="group in groups" :key="group.id">
+      <groupcard :group="group" />
+    </div>
   </v-layout>
 </template>
 
 <script>
+import groupcard from '~/components/groups/group_card.vue'
 
 export default {
+  components: {
+    groupcard
+  },
   data () {
-    return {}
+    return {
+      groups: [
+        {
+          id: 1,
+          title: 'financeiro',
+          items: ['office', 'powerbi']
+        },
+        {
+          id: 2,
+          title: 'people',
+          items: ['office', 'powerbi', 'jazzhr']
+        },
+        {
+          id: 3,
+          title: 'dev',
+          items: ['pycharm']
+        }
+      ]
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 </style>
